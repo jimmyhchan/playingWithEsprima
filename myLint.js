3f85a8d68b25ccfceb34fde2a4d85a5a88c79d3c
Jimmy Chan <jimmyhchan@gmail.com>
2013-11-11 17:56:28 -0800

    a simple example of esprima.parse with the comment option set to true

--- myLint.js
+++ myLint.js
@@ -1,7 +1,35 @@
-var esprima = require('esprima');
+var esprima = require('esprima'),
+    ast;
 
-if (esprima) {
-  console.log('yes esprima');
-} else {
-  console.log('no esprima');
+
+// @see http://esprima.org/
+
+// simple parse ussage @see http://esprima.org/doc/index.html
+ast = esprima.parse(
+    [ '// a simple comment',
+    'var answer = 42'
+    ].join('\n'), 'comment', true);
+
+
+console.log(JSON.stringify(ast));
+/**
+{
+    "type": "Program",
+    "body": [{
+        "type": "VariableDeclaration",
+        "declarations": [{
+            "type": "VariableDeclarator",
+            "id": {
+                "type": "Identifier",
+                "name": "answer"
+            },
+            "init": {
+                "type": "Literal",
+                "value": 42
+            }
+        }],
+        "kind": "var"
+    }]
 }
+
+*/
